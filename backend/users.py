995ef255fb11ace3cba3a7ec18955cd5fbdfe9c5
gooeye <uniconpatio@gmail.com>
2024-09-17 23:19:14 +0800

    feat: created batch functions and fixed create_user

--- backend/users.py
+++ backend/users.py
@@ -14,17 +14,13 @@ def create_user():
         if not user_data:
             return jsonify({"error": "No data provided"}), 400
         
-        # Validate required fields
-        if 'id' not in user_data:
-            return jsonify({"error": "Missing 'id'"}), 400
-        
         # Check if the user already exists
         user_ref = db.collection('users').document(user_data['id'])
         if user_ref.get().exists:
             return jsonify({"error": "User already exists"}), 400
 
         # Create a new user document in Firestore
-        user_ref = db.collection('users').document(user_id)
+        user_ref = db.collection('users').document(user_data['id'])
         user_ref.set({
             'id': user_data['id'],
             'country': user_data['country'],
@@ -39,6 +35,43 @@ def create_user():
         
         return jsonify({"message": "User created", "user_id": user_data['id']}), 201
 
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@users_bp.route('/create_users', methods=['POST'])
+def create_users():
+    # takes an array of users and creates multiple users
+    try:
+        # Get the user data from the request
+        count = 0
+        users_data = request.json
+        if not users_data:
+            return jsonify({"error": "No data provided"}), 400
+        # batch write data
+        
+        batch = db.batch()
+        for user_data in users_data:
+            user_ref = db.collection('users').document(user_data['id'])
+
+            batch.set(user_ref, {
+                'country': user_data['country'],
+                'dept': user_data['dept'],
+                'email': user_data['email'],
+                'first_name': user_data['first_name'],
+                'last_name': user_data['last_name'],
+                'position': user_data['position'],
+                'role': user_data['role'],
+                'rpt_manager': user_data['rpt_manager'],
+            })
+            print(f"{count}/{len(users_data)} | current user: {user_data['id']}")
+            count += 1
+            if count % 500 == 0:
+                batch.commit()
+                batch = db.batch()
+                print(f"committed {count} users")
+        if count % 500 != 0: batch.commit()
+
+        return jsonify({"message": f"{count} users created"}), 201
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -78,10 +111,6 @@ def update_user(user_id):
         if not user_data:
             return jsonify({"error": "No data provided"}), 400
         
-        # Validate required fields
-        if 'id' not in user_data:
-            return jsonify({"error": "Missing 'id'"}), 400
-        
         # Update the user in Firestore
         user_ref = db.collection('users').document(user_id)
         user_ref.update({
@@ -108,6 +137,27 @@ def delete_user(user_id):
         user_ref.delete()
         
         return jsonify({"message": "User deleted", "user_id": user_id}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@users_bp.route('/delete_all_users', methods=['DELETE'])
+def delete_all_users():
+    try:
+        count = 0
+        # Delete all users from Firestore
+        users = db.collection('users').stream()
+        batch = db.batch()
+        for user in users:
+            batch.delete(user.reference)
+            count += 1
+            if count % 500 == 0:
+                batch.commit()
+                batch = db.batch()
+                print(f"committed {count} users")
+        if count % 500 != 0: batch.commit()
+        
+        return jsonify({"message": f"Deleted {count} users"}), 200
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
