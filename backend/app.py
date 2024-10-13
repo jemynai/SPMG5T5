dfe9278022192e7a88e73e8603ea5c9490d0db64
RayneAng <144549519+RayneAng@users.noreply.github.com>
2024-10-13 13:24:02 +0800

    Create mnger_view_ttbl.py instead of adding to arrangements.py
    mnger_view_ttbl function to fetch the timetable for a given department in real-time.
    Manager View Timetable (/mnger_view_ttbl): Allows managers to view the team timetable based on the department_id and optionally filter by employee status (e.g., office, home).
    The route will return a list of arrangements for team members within a department and optionally filter by their in-office or work-from-home status.

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from users import users_bp
 from arrangements import arrangement_bp
 from withdrawals import withdrawal_bp
+from mngr_view_ttbl import mngr_view_bp 
 from apply import apply_bp 
 from flask_cors import CORS
 
@@ -15,6 +16,7 @@ app.register_blueprint(users_bp)
 app.register_blueprint(arrangement_bp)
 app.register_blueprint(withdrawal_bp)
 app.register_blueprint(apply_bp) 
+app.register_blueprint(mngr_view_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
